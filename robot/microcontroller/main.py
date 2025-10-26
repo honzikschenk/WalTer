8cee6fcf59d1d037279ca9ccd050d6d49ded78eb
FujiBookPro <maczone06@gmail.com>
2025-10-26 02:11:12 -0400

    Implement DriveCellController on Pi Pico

--- robot/microcontroller/main.py
+++ robot/microcontroller/main.py
@@ -1,11 +1,34 @@
 from machine import Pin
 
 class DriveCellControl:
+    MOTOR_PINS = {0: (0, 1), 1: (2, 3)}
+
     def __init__(self, motor_number=0):
-        pass
+        self.number = motor_number
+        (p1, p2) = DriveCellControl.MOTOR_PINS[motor_number]
+
+        self.pwm1 = PWM(Pin(p1))
+        self.pwm2 = PWM(Pin(p2))
+
+        self.pwm1.freq(1000)
+        self.pwm2.freq(1000)
         
-    def control_drive_cell(self, direction, speed):
-        print(f"Driving {direction} at speed {speed}")
+        # default to coast mode
+        self.pwm1.duty_u16(0)
+        self.pwm2.duty_u16(0)
+        
+    def control_drive_cell(self, speed):
+        print(f"Driving motor {self.number} at speed {speed}")
+
+        if speed > 0:
+            self.pwm1.duty_u16(int(speed * 65535))
+            self.pwm2.duty_u16(0)
+        if speed < 0:
+            self.pwm1.duty_u16(0)
+            self.pwm2.duty_u16(int(-1 * speed * 65535))
+        else:
+            self.pwm1.duty_u16(0)
+            self.pwm2.duty_u16(0)
 
 class OLEDDisplay:
     def __init__(self):
